c9a65769c5f43e2fffe6f8c30ced5ca740274516
Flandia Yingman <me@flandia.dev>
2024-02-18 00:01:57 +0800

    feat: v1.0

--- src/api.js
+++ src/api.js
@@ -1,23 +1,53 @@
-const actionsHttpClient = require('@actions/http-client')
+const axios = require('axios')
+const axiosRetry = require('axios-retry')
 const { parse } = require('node-html-parser')
 
 const base = 'https://w5.ab.ust.hk/wcq/cgi-bin'
 
-const client = new actionsHttpClient.HttpClient(undefined, undefined, {
-  allowRedirects: false,
-  allowRetries: true,
-  maxRetries: 3,
+const client = axios.create()
+axiosRetry.default(client, {
+  retries: 30,
+  retryDelay: axiosRetry.exponentialDelay
 })
 
-async function fetchTerm() {
-  const response = await client.get(`${base}/`)
-  const location = response.message.headers.location
-  return new URL(location, base).pathname.split('/')[3]
+/**
+ * Parse term code to human-readable format.
+ * @param {string} code Term code.
+ * @returns {string} Human-readable term.
+ */
+function parseTerm (code) {
+  const SEASON_MAP = {
+    10: 'Fall',
+    20: 'Winter',
+    30: 'Spring',
+    40: 'Summer'
+  }
+
+  const suffix = parseInt(code.slice(0, 2), 10)
+  const year = `20${suffix}-${suffix + 1}`
+
+  const season = SEASON_MAP[code.slice(2, 4)]
+
+  return `${year} ${season}`
 }
 
-async function fetchSubjects(term) {
+async function fetchTerm () {
+  const response = await client.get(`${base}/`, {
+    validateStatus: status => status === 302,
+    maxRedirects: 0
+  })
+  const location = response.headers.location
+  const code = new URL(location, base).pathname.split('/')[3]
+  const name = parseTerm(code)
+  return {
+    code,
+    name
+  }
+}
+
+async function fetchSubjects (term) {
   const response = await client.get(`${base}/${term}/`)
-  const html = await response.readBody()
+  const html = response.data
   const root = parse(html)
   const subjectElements = root.querySelectorAll('.depts > a')
   return subjectElements.map(element => ({
@@ -27,9 +57,9 @@ async function fetchSubjects(term) {
   }))
 }
 
-async function fetchPage(subject) {
+async function fetchPage (subject) {
   const response = await client.get(subject.url)
-  const html = await response.readBody()
+  const html = response.data
   return {
     name: subject.name,
     term: subject.term,
@@ -38,7 +68,7 @@ async function fetchPage(subject) {
 }
 
 module.exports = {
-  fetchCurrentTerm: fetchTerm,
+  fetchTerm,
   fetchSubjects,
   fetchPage
 }
