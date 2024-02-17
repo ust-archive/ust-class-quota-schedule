918fa5b7f121afcbbfc2f2b3e88d8f383e4abdcb
Flandia Yingman <me@flandia.dev>
2024-02-18 00:01:57 +0800

    feat: v1.0

--- src/main.js
+++ src/main.js
@@ -1,38 +1,68 @@
 const core = require('@actions/core')
-const { fetchPage, fetchSubjects, fetchCurrentTerm } = require('./api')
+const { fetchPage, fetchSubjects, fetchTerm } = require('./api')
 const fs = require('fs/promises')
 const { convert } = require('./parse')
+const path = require('path')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-async function run() {
+const DATA_DIR = 'data'
+
+async function pull () {
+  core.info('Fetching the current term...')
+  const termObj = await fetchTerm()
+  const term = termObj.code
+  core.info(`Fetched the current term: ${termObj.name}.`)
+
+  core.info('Fetching subjects...')
+  const subjects = await fetchSubjects(term)
+  core.info(`Fetched ${subjects.length} subjects.`)
+
+  core.info(`Fetching ${subjects.length} pages...`)
+  const pages = await Promise.all(subjects.map(fetchPage))
+  core.info(`Fetched ${pages.length} page.`)
+
+  await Promise.all(
+    pages.map(async page => {
+      const base = `${DATA_DIR}/${termObj.name}/`
+      try {
+        await fs.access(base)
+      } catch (e) {
+        await fs.mkdir(base, { recursive: true })
+      }
+      await fs.writeFile(`${base}/${page.name}.html`, page.html)
+      core.info(`Written src: ${page.term}/${page.name}.`)
+    })
+  )
+  await fs.writeFile(`${DATA_DIR}/current-term.txt`, termObj.name)
+
+  return [termObj, pages]
+}
+
+function slimize (courseObj) {
+  return {
+    program: courseObj.program,
+    code: courseObj.code,
+    name: courseObj.name,
+    units: courseObj.units,
+    description: courseObj.info.description,
+    sections: courseObj.sections.map(section => ({
+      section: section.section,
+      number: section.number,
+      schedule: section.schedule,
+      instructors: section.instructors,
+      room: section.room,
+      quota: [
+        section.quota,
+        section.enroll,
+        section.available,
+        section.waitlist
+      ]
+    }))
+  }
+}
+
+async function run () {
   try {
-    core.info('Fetching current term...')
-    const term = await fetchCurrentTerm()
-    core.info(`Fetched current term: ${term}.`)
-
-    core.info('Fetching subjects...')
-    const subjects = await fetchSubjects(term)
-    core.info(`Fetched ${subjects.length} subjects.`)
-
-    core.info(`Fetching ${subjects.length} pages...`)
-    const pages = await Promise.all(subjects.map(fetchPage))
-    core.info(`Fetched ${pages.length} page.`)
-
-    await Promise.all(
-      pages.map(async page => {
-        const base = `${page.term}/src`
-        try {
-          await fs.access(base)
-        } catch (e) {
-          await fs.mkdir(base, { recursive: true })
-        }
-        await fs.writeFile(`${base}/${page.name}.html`, page.html)
-        core.info(`Written src: ${page.term}/${page.name}.`)
-      })
-    )
+    const [term, pages] = await pull()
 
     core.info(`Converting ${pages.length} pages.`)
     const data = pages.map(page => ({
@@ -42,20 +72,22 @@ async function run() {
     }))
     core.info(`Converted ${data.length} pages.`)
 
-    await Promise.all(
-      data.map(async datum => {
-        const base = `${datum.term}`
-        try {
-          await fs.access(base)
-        } catch (e) {
-          await fs.mkdir(base, { recursive: true })
-        }
-        await fs.writeFile(
-          `${base}/${datum.name}.json`,
-          JSON.stringify(datum.data, null, 2)
-        )
-        core.info(`Written JSON: ${datum.term}/${datum.name}.`)
-      })
+    await fs.mkdir(DATA_DIR, { recursive: true })
+
+    const jsonObj = Object.fromEntries(
+      data.map(datum => [datum.name, datum.data])
+    )
+    await fs.writeFile(
+      path.join(DATA_DIR, `${term.name}.json`),
+      JSON.stringify(jsonObj, null, 2)
+    )
+
+    const slimJsonObj = Object.fromEntries(
+      data.map(datum => [datum.name, datum.data.map(slimize)])
+    )
+    await fs.writeFile(
+      path.join(DATA_DIR, `${term.name} Slim.json`),
+      JSON.stringify(slimJsonObj, null, 2)
     )
   } catch (error) {
     core.setFailed(error.message)
--- src/parse.js
+++ src/parse.js
@@ -1,13 +1,15 @@
+const { Locale } = require('@js-joda/locale_en-us')
 const { parse } = require('node-html-parser')
 const { tabletojson } = require('tabletojson')
+const { DayOfWeek, LocalTime, DateTimeFormatter } = require('@js-joda/core')
 
-function convert(pageHTML) {
+function convert (pageHTML) {
   const root = parse(pageHTML)
   const courseElements = root.querySelectorAll('.course')
   return courseElements.map(convertCourse)
 }
 
-function convertCourse(courseEl) {
+function convertCourse (courseEl) {
   const title = convertTitle(courseEl.querySelector('h2').text)
   const info = convertInfo(courseEl.querySelector('.courseattr'))
   const attrs = courseEl.querySelectorAll('.attrword').map(convertAttr)
@@ -20,7 +22,7 @@ function convertCourse(courseEl) {
   }
 }
 
-function convertTitle(title) {
+function convertTitle (title) {
   // ACCT 1010 - Accounting, Business and Society (3 units)
   // program: ACCT
   // code: 1010
@@ -40,7 +42,7 @@ function convertTitle(title) {
   }
 }
 
-function convertInfo(infoEl) {
+function convertInfo (infoEl) {
   const rows = infoEl.querySelectorAll('tr:not(tr tr)')
   const data = {}
   for (const row of rows) {
@@ -53,19 +55,74 @@ function convertInfo(infoEl) {
   return data
 }
 
-function convertAttr(attrEl) {
+function convertAttr (attrEl) {
   return {
     title: attrEl.querySelector('span').text,
     description: attrEl.querySelector('div').text
   }
 }
 
-function convertSections(sectionsEl) {
+/**
+ * Parses section string (e.g., L1 (1023), LA1 (1979)) into an object.
+ * @param {string} strSection
+ * @return {{section: string, type: string}}
+ */
+function parseSection (strSection) {
+  const regex = /(\w+)\s+\((\d+)\)/
+  const match = strSection.match(regex)
+  if (match) {
+    return {
+      section: match[1],
+      number: parseInt(match[2], 10)
+    }
+  } else {
+    throw new Error(`Invalid section string format: ${strSection}.`)
+  }
+}
+
+/**
+ * Parse Date & Time string (e.g., Tu 01:30PM - 03:20PM, WeFr 04:30PM - 05:20PM, MoWeFr 10:00AM - 10:50AM) into an object.
+ * @param {string} strDateTime
+ * @return {{day: string, start: string, end: string}[]}
+ */
+function parseDateTime (strDateTime) {
+  try {
+    if (strDateTime === 'TBA') return []
+
+    const DAY_OF_WEEK = {
+      Mo: DayOfWeek.MONDAY,
+      Tu: DayOfWeek.TUESDAY,
+      We: DayOfWeek.WEDNESDAY,
+      Th: DayOfWeek.THURSDAY,
+      Fr: DayOfWeek.FRIDAY,
+      Sa: DayOfWeek.SATURDAY,
+      Su: DayOfWeek.SUNDAY
+    }
+    const FORMATTER = DateTimeFormatter.ofPattern('hh:mma').withLocale(
+      Locale.ENGLISH
+    )
+
+    const [strDays, strStart, strEnd] = strDateTime.split(/[\s-]+/)
+    const days = strDays.match(/.{2}/g).map(s => DAY_OF_WEEK[s])
+    const start = LocalTime.parse(strStart, FORMATTER)
+    const end = LocalTime.parse(strEnd, FORMATTER)
+
+    return days.map(day => ({
+      day: day.toString(),
+      start: start.format(DateTimeFormatter.ISO_LOCAL_TIME),
+      end: end.format(DateTimeFormatter.ISO_LOCAL_TIME)
+    }))
+  } catch (e) {
+    return []
+  }
+}
+
+function convertSections (sectionsEl) {
   const [sections] = tabletojson.convert(sectionsEl.outerHTML, {
     stripHtmlFromCells: false
   })
-  return sections.map(section => {
-    function convertPeople(instructorEl) {
+  return sections.flatMap(section => {
+    function convertPeople (instructorEl) {
       const instructors = instructorEl.querySelectorAll('a')
       if (instructors.length) {
         return instructors.map(a => a.text).filter(s => s !== '')
@@ -73,9 +130,10 @@ function convertSections(sectionsEl) {
         return [instructorEl.text]
       }
     }
-    const instructor = convertPeople(parse(section['Instructor']))
+
+    const instructor = convertPeople(parse(section.Instructor))
     const ta = convertPeople(parse(section['TA/IA/GTA']))
-    const remarks = parse(section['Remarks'])
+    const remarks = parse(section.Remarks)
       .querySelectorAll('.popup')
       .map(a => a.text)
       .flatMap(s => s.split(/\s*>\s*/))
@@ -88,9 +146,9 @@ function convertSections(sectionsEl) {
       } else {
         return parseInt(quotaEl.text, 10)
       }
-    })(section['Quota'])
+    })(section.Quota)
     const quotaDetail = (quotaHTML => {
-      function convertDetail(detailEl) {
+      function convertDetail (detailEl) {
         // Quota/Enrol/Avail
         // ACCT: 68/43/25
         const lines = detailEl.text.split('\n')
@@ -115,32 +173,22 @@ function convertSections(sectionsEl) {
           ...convertDetail(detailEl)
         }
       }
-    })(section['Quota'])
+    })(section.Quota)
 
-    return {
-      // eslint-disable-next-line prettier/prettier
-      'Section': section['Section'],
-      // eslint-disable-next-line prettier/prettier
-      'Date & Time': section['Date & Time'],
-      // eslint-disable-next-line prettier/prettier
-      'Room': section['Room'],
-      // eslint-disable-next-line prettier/prettier
-      'Instructor': instructor,
-      // eslint-disable-next-line prettier/prettier
-      'TA/IA/GTA': ta,
-      // eslint-disable-next-line prettier/prettier
-      'Quota': quota,
-      // eslint-disable-next-line prettier/prettier
-      'Enrol': parseInt(parse(section['Enrol']).text, 10),
-      // eslint-disable-next-line prettier/prettier
-      'Avail': parseInt(parse(section['Avail']).text, 10),
-      // eslint-disable-next-line prettier/prettier
-      'Wait': parseInt(parse(section['Wait']).text, 10),
-      // eslint-disable-next-line prettier/prettier
-      'QuotaDetail': quotaDetail,
-      // eslint-disable-next-line prettier/prettier
-      'Remarks': remarks
-    }
+    const schedules = parseDateTime(section['Date & Time'])
+    return schedules.map(schedule => ({
+      ...parseSection(section.Section),
+      schedule,
+      room: section.Room,
+      instructors: instructor,
+      assistants: ta,
+      quota,
+      enroll: parseInt(parse(section.Enrol).text, 10),
+      available: parseInt(parse(section.Avail).text, 10),
+      waitlist: parseInt(parse(section.Wait).text, 10),
+      quotaDetail,
+      remarks
+    }))
   })
 }
 
